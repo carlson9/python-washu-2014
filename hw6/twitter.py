07ae242d4b6ecdb03c445553f7158c02ce29c612
carlson9 <carlson.dav@gmail.com>
2014-08-19 16:23:40 -0500

    hw6
    second part takes a very long time

--- hw6/twitter.py
+++ hw6/twitter.py
@@ -1,4 +1,5 @@
 import time
+from datetime import timedelta
 followers = api.followers_ids('mcdickenson')
 followers_count = 0
 i=0
@@ -10,7 +11,22 @@ while i<len(followers):
             most_followed = str(user.name)
         i+=1
     except: time.sleep(.25)
-#Stratos Safioleas 49058
+#Virgin America 554395
+followed = api.friends_ids('mcdickenson')
+i = 0
+time_between = timedelta(100)
+while i<len(followed):
+    try:
+        user = api.get_user(followed[i])
+        tweets = user.timeline()
+        if len(tweets) == 20:
+            time_delta = tweets[0].created_at - tweets[19].created_at
+            if time_delta < time_between:
+                time_between = time_delta
+                most_active = str(user.name)
+        i+=1
+    except: time.sleep(.25)
+
 
 
 
